dbfe11579932df75b85a3ba9334a4e6615171ad2
Mert Yücesoy <mert.yucesoy1@gmail.com>
2022-07-12 14:32:13 +0300

    App.js updated with CardTable

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import CardTableCSS from "./components/card/Card.module.css";
+
+import CardTable from "./components/card/Card";
+
+
+
 const App = () => {
-  return (
-    <h1>React Dashboard</h1>
-  );
+   return (
+    <div className={CardTableCSS.table}>
+      <CardTable />
+    </div>
+   )
 }
 
 export default App;
