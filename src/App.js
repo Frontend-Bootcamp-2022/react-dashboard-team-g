d1201a29a5c85b0c2f34f10bf5580e9119e5323d
TARIK BAYRAM <tkbayram7@gmail.com>
2022-07-13 21:01:01 +0300

    Update: Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import HeaderTop from "./components/Header/HeaderTop";
 import CardTableCSS from "./components/card/Card.module.css";
 import CardTable from "./components/card/Card";
+import Sidebar from "./components/Sidebar/Sidebar";
 import "./App.scss";
 
 const App = () => {
   return (
     <div className="App">
+      <Sidebar></Sidebar>
       <HeaderTop />
       <div className={CardTableCSS.table}>
         <CardTable />
---
+++ src/components/Sidebar/Box.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from "./Sidebar.module.scss";
+
+
+const Box = (props) => {
+  return (
+    <div className={style.box}>
+        {props.img}
+        {props.name}
+    </div>
+  )
+}
+
+export default Box
